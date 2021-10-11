295891d7d175e730ee8246cd310f6e23ab9a7d7b
wazovskii <veroni3ka2000@gmail.com>
2021-10-11 21:21:02 +0700

    degrees converter

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,11 +1,109 @@
-class Welcome
-  attr_accessor :name
+class Temperature
+  attr_reader :result
 
-  def initialize(name)
-    @name = name
+  def initialize(in_lettter, out_letter, num)
+    @in_lettter = in_lettter
+    @out_letter = out_letter
+    @num = num
   end
 
-  def greet
-    "Hello #{@name}"
+  def from_f
+    case @out_letter
+    when 'F'
+      @num.to_s
+    when 'C'
+      (5.0 / 9.0 * (@num - 32)).to_s
+    when 'K'
+      ((@num + 459.67) * 5.0 / 9.0).to_s
+    else 'wrong output letter'
+    end
+  end
+
+  def from_c
+    case @out_letter
+    when 'F'
+      (@num *  9.0 / 5.0 + 32).to_s
+    when 'C'
+      @num.to_s
+    when 'K'
+      (@num + 273.15).to_s
+    else 'wrong output letter'
+    end
+  end
+
+  def from_k
+    case @out_letter
+    when 'F'
+      (@num * 9.0 / 5.0 - 459.67).to_s
+    when 'C'
+      (@num - 273.15).to_s
+    when 'K'
+      @num.to_s
+    else 'wrong output letter'
+    end
+  end
+
+  def calculate
+    @result = case @in_lettter
+              when 'F'
+                from_f
+              when 'C'
+                from_c
+              when 'K'
+                from_k
+              else 'wrong input letter'
+              end
+    @result.to_s
+  end
+
+  def print
+    puts @result
+  end
+end
+
+class Context
+  attr_accessor :state, :temperature
+  private :state
+
+  def initialize(state, temperature)
+    transition_to(state, temperature)
+  end
+
+  def transition_to(state, temperature)
+    @state = state
+    @state.temperature = temperature
+    @state.context = self
+  end
+
+  def request1
+    @state.handle1
+  end
+end
+
+class State
+  attr_accessor :context, :temperature
+
+  def handle1
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
+
+class InitState < State
+  def handle1
+    @context.transition_to(CalculationState.new, temperature)
+  end
+end
+
+class CalculationState < State
+  def handle1
+    temperature.calculate
+    @context.transition_to(PrintState.new, temperature)
+  end
+end
+
+class PrintState < State
+  def handle1
+    temperature.print
+    @context.transition_to(InitState.new, temperature)
   end
 end
--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -1,9 +1,148 @@
 require './lib/welcome'
 
-RSpec.describe Welcome do
-  describe '#hello' do
-    subject { Welcome.new('Harry').greet }
+RSpec.describe Temperature do
+  describe '#From D to C calculation value 0' do
+    subject do
+      temperature = Temperature.new('D', 'C', 0)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
 
-    it { is_expected.to eq 'Hello Harry' }
+    it { is_expected.to eq 'wrong input letter' }
+  end
+  describe '#From C to M calculation value 0' do
+    subject do
+      temperature = Temperature.new('C', 'M', 0)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
+
+    it { is_expected.to eq 'wrong output letter' }
+  end
+  describe '#From C to F calculation value 3' do
+    subject do
+      temperature = Temperature.new('C', 'F', 3)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
+
+    it { is_expected.to eq '37.4' }
+  end
+  describe '#From C to K calculation value 30' do
+    subject do
+      temperature = Temperature.new('C', 'K', 30)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
+
+    it { is_expected.to eq '303.15' }
+  end
+  describe '#From C to C calculation value 5' do
+    subject do
+      temperature = Temperature.new('C', 'C', 5)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
+
+    it { is_expected.to eq '5' }
+  end
+  describe '#From K to F calculation value 5' do
+    subject do
+      temperature = Temperature.new('K', 'F', 5)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
+
+    it { is_expected.to eq '-450.67' }
+  end
+  describe '#From K to C calculation value 50' do
+    subject do
+      temperature = Temperature.new('K', 'C', 50)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
+
+    it { is_expected.to eq '-223.14999999999998' }
+  end
+  describe '#From K to K calculation value 555' do
+    subject do
+      temperature = Temperature.new('K', 'K', 555)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
+
+    it { is_expected.to eq '555' }
+  end
+
+  describe '#From F to K calculation value 10' do
+    subject do
+      temperature = Temperature.new('F', 'K', 10)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
+
+    it { is_expected.to eq '260.92777777777775' }
+  end
+  describe '#From F to C calculation value 100' do
+    subject do
+      temperature = Temperature.new('F', 'K', 100)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
+
+    it { is_expected.to eq '310.9277777777778' }
+  end
+  describe '#From F to F calculation value 1000' do
+    subject do
+      temperature = Temperature.new('F', 'F', 1000)
+      context = Context.new(InitState.new, temperature)
+      context.request1
+      context.request1
+      temperature.result
+    end
+
+    it { is_expected.to eq '1000' }
+  end
+  describe '#From F func' do
+    subject do
+      temperature = Temperature.new('F', 'M', 1000)
+      temperature.from_f
+      # temperature.result
+    end
+    it { is_expected.to eq 'wrong output letter' }
+  end
+  describe '#From C func' do
+    subject do
+      temperature = Temperature.new('C', 'M', 1000)
+      temperature.from_c
+    end
+    it { is_expected.to eq 'wrong output letter' }
+  end
+  describe '#From K func' do
+    subject do
+      temperature = Temperature.new('K', 'M', 1000)
+      temperature.from_k
+    end
+    it { is_expected.to eq 'wrong output letter' }
   end
 end
